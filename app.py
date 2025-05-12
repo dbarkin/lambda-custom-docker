8108f024e00273051adfde6a0ca51f65d31f4f0c
Dmitrij Barkin <dbarkin@users.noreply.github.com>
2025-05-12 13:18:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ def lambda_handler(event, context):
     # Copy the script to /tmp directory
     # Set the executable permission to the script
     # Run the script
-    result = subprocess.run(["atlas", "--version"], capture_output=True, text=True)
+    result = subprocess.run(["python", "-c", "import time; time.sleep(1)"], capture_output=True, text=True, check=True)
+    print('output: ', result.stdout)
+    print('error: ', result.stderr)
+    #result = subprocess.run(["atlas", "--version"], capture_output=True, text=True)
     return {
         'statusCode': 200,
         'body': result.stdout
