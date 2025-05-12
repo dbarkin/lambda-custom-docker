86264bbf44b3de9d64fd595a01704f42de2438ad
Dmitrij Barkin <dbarkin@users.noreply.github.com>
2025-05-12 13:40:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     #result = subprocess.run(["python", "-c", "import time; time.sleep(1)"], capture_output=True, text=True, check=True)
     #print('output: ', result.stdout)
     #print('error: ', result.stderr)
-    result = subprocess.run(["/usr/local/bin/atlas", "--version"], capture_output=True, text=True, check=True)
+    result = subprocess.run(["/usr/bin/atlas", "--version"], capture_output=True, text=True, check=True)
     print('output: ', result.stdout)
     print('error: ', result.stderr)
     
