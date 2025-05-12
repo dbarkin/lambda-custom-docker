7a4d3fb6b308f00aa979959bbb6b6cf94fa037a2
Dmitrij Barkin <dbarkin@users.noreply.github.com>
2025-05-12 12:13:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,6 @@ import json
 import boto3
 import os
 
-# env variables for aws client
-S3_BUCKET = os.getenv('S3_BUCKET')
-S3_KEY = os.getenv('S3_KEY')
-S3_SECRET = os.getenv('S3_SECRET')
-S3_REGION = 'ca-central-1'
-
-s3 = boto3.client(
-    "s3", 
-    aws_access_key_id=S3_KEY,
-    aws_secret_access_key=S3_SECRET, 
-    region_name = S3_REGION)
-
 def handler(event, context):
     
     event = json.loads(event['body'])
