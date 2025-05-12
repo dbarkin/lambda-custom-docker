fce95e0b8ff77d8323bcf4f139eded37ae0ff5a2
Dmitrij Barkin <dbarkin@users.noreply.github.com>
2025-05-12 13:04:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import json
 import boto3
 import os
+import subprocess
+import shutil
 
 def lambda_handler(event, context):
-    
-    event = json.loads(event['Records'][0]['body'])
-    print(event, type(event))
-    try:        
-        return json.dumps({
-            'statusCode':200,
-            'key_phrases': 'test'})
-
-    except Exception as e:
-        print(e)
-        print("Something went wrong.")
-        return {
-            'statusCode':404,
-            'key_phrases': e}
+    # Copy the script to /tmp directory
+    # Set the executable permission to the script
+    # Run the script
+    result = subprocess.run(["atlas", "--version"], capture_output=True, text=True)
+    return {
+        'statusCode': 200,
+        'body': result.stdout
+    }
